a8289e66f13266225a365b86622c0d8ac80884d3
JeremieC <certain.j@live.fr>
2021-03-27 23:18:56 +0100

    modal cookie & repop cookie disagree

--- js/main.js
+++ js/main.js
@@ -5,21 +5,27 @@
 
 //Importation of Class Folder
 import {Crypto as CryptoFnc} from "./utils/crypto.js";
-import {Cookie as CookieoFnc} from "./utils/cookie.js";
-
+import {Cookie as CookieFnc} from "./utils/cookie.js";
 import {config} from "./config.crypto.js";
 
 let dataDefaultCrypto = []
 
+const elements = {
+  mainTable: document.body.querySelector('.main-table'),
+  btnDecline: document.body.querySelector('#btn-decline-cookie'),
+  btnAccept: document.body.querySelector('#btn-accept-cookie'),
+  modalCookie: document.body.querySelector('#modal-for-init-cookie'),
+  modalRePopCookie: document.body.querySelector('#modal-for-repop-cookie'),
+}
 
 //If cookie is defined
 //Means that the cookie is accepted by user to keep his save of crypto data
 //Use the editedCryptoTable to edit the list of crypto display
-if(typeof CookieoFnc.getCookie('testForUserCookie') === 'string') {
+if (typeof CookieFnc.getCookie('testForUserCookie') === 'string') {
 
-  let checkCookie = JSON.parse(CookieoFnc.getCookie('testForUserCookie'))
+  let checkCookie = JSON.parse(CookieFnc.getCookie('testForUserCookie'))
 
-  if(checkCookie !== '' && typeof checkCookie === 'object') {
+  if (checkCookie !== '' && typeof checkCookie === 'object') {
     console.log(checkCookie);
     let userCryptoList = checkCookie.listCrypto
 
@@ -33,38 +39,36 @@ if(typeof CookieoFnc.getCookie('testForUserCookie') === 'string') {
   //If the user accepted the cookie, initiate the data by setting the cookie with the editedData
 } else {
 
-  if(localStorage.getItem('dataCrypto')) {
+  if (localStorage.getItem('dataCrypto')) {
     //Get the data save to reuse it in html instead of redoing the logic data
     let retrievedData = JSON.parse(localStorage.getItem('dataCrypto'));
 
     //Delete the raw data and inject the html from object
-    document.body.querySelector('.main-table').remove()
+    elements.mainTable.remove()
     document.body.querySelector('.resume').insertAdjacentHTML('afterend', retrievedData.htmlGenerated)
 
   } else {
 
     CryptoFnc.createCryptoData(config.defaultCryptos, dataDefaultCrypto)
-    //TODO - DO SOMETHING WITH VALUE SAVED ? DO RETURN ??
     let generatedData = CryptoFnc.construcTableWithData(dataDefaultCrypto)
 
     //Save in localstorage to avoid repeat object instanciation
     let dataToLocalStorage = {
-      generatedData: generatedData ? generatedData : 'empty',
-      htmlGenerated: document.body.querySelector('.main-table').outerHTML
+      htmlGenerated: elements.mainTable.outerHTML
     }
 
     localStorage.setItem('dataCrypto', JSON.stringify(dataToLocalStorage))
   }
+
+  //COOKIES
+  CookieFnc.disagreeCookie(elements)
+  elements.btnAccept.addEventListener('click', (ev) => {
+    CookieFnc.saveUserInCookie(dataDefaultCrypto, 1)
+  })
+
+  //TODO - BTN ACTION ACCEPT COOKIE - THEN ADD CLASS TO MODAL COOKIE - CHECK IF COOKIE IS SET THEN HIDDEN CLASS TO MODAL COOKIE - LOGIC FOR COOKIE DATA TO TABLE EDITED CRYPTO - BTN ACTIONS TO EDIT&DELETE&NEW
+
 }
-
-
-//On accepted Cookie
-//Save into cookie the defaultCryptoList
-
-
-
-
-
 
 //************* Front Behavior DOM actions - Treat Data *************//
 
@@ -80,17 +84,16 @@ for (let i = 0; i < listTrElements.length; i++) {
   listTrElements[i].lastElementChild.insertAdjacentElement('afterend', btnToTr)
 }
 
-
 //BTN DELETE
 document.body.querySelector('#btn-del-crypto').addEventListener('click', (ev) => {
-  if(modal.dataset.idCrypto && typeof modal.dataset.idCrypto === 'string') {
+  if (modal.dataset.idCrypto && typeof modal.dataset.idCrypto === 'string') {
     console.log(modal.dataset.idCrypto)
   }
 })
 
 //BTN ADD TO FAV
 document.body.querySelector('#btn-add-to-fav').addEventListener('click', (ev) => {
-  if(modal.dataset.idCrypto && typeof modal.dataset.idCrypto === 'string') {
+  if (modal.dataset.idCrypto && typeof modal.dataset.idCrypto === 'string') {
     console.log(modal.dataset.idCrypto)
   }
 })
@@ -99,36 +102,9 @@ document.body.querySelector('#btn-add-to-fav').addEventListener('click', (ev) =>
 document.body.querySelectorAll('.btn-add-new').forEach(elmt => {
   elmt.addEventListener('click', (ev) => {
     ev.preventDefault()
-    if(modal.dataset.idCrypto && typeof modal.dataset.idCrypto === 'string') {
+    if (modal.dataset.idCrypto && typeof modal.dataset.idCrypto === 'string') {
       console.log(modal.dataset.idCrypto)
     }
   })
 
 })
-
-document.body.querySelector('#btn-decline-cookie').addEventListener('click', (e) => {
-  let modal = document.body.querySelector('#modal-for-init-cookie')
-  let modalRepop = document.body.querySelector('#modal-for-repop-cookie')
-
-  modal.classList.add('hidden-cookie')
-  setTimeout(() => {
-    modal.style.display = 'none';
-    modalRepop.style.display = 'block'
-  },
-    1500)
-})
-
-document.body.querySelector('#modal-for-repop-cookie').addEventListener('click', (e) => {
-  let modal = document.body.querySelector('#modal-for-init-cookie')
-
-  modal.classList.remove('hidden-cookie')
-
-  setTimeout(() => {
-    //modal.style.display = 'block';
-      document.body.querySelector('#modal-for-repop-cookie').style.display = 'none'
-  },
-    1500)
-})
-
-
-
--- js/utils/cookie.js
+++ js/utils/cookie.js
@@ -1,11 +1,5 @@
 export class Cookie {
 
-  elements = {
-    btnDecline : document.body.querySelector('#btn-decline-cookie'),
-    btnAccept : document.body.querySelector('#btn-accept-cookie'),
-    modalCookie : document.body.querySelector('#modal-for-init-cookie'),
-  }
-
   /**
    * @param dataDefaultCrypto
    * @param duration
@@ -19,7 +13,7 @@ export class Cookie {
       listCrypto: [dataDefaultCrypto]
     }
 
-    setCookie('testForUserCookie', JSON.stringify(userCookie), parseInt(duration))
+    this.setCookie('InfoCryptoUsername', JSON.stringify(userCookie), parseInt(duration))
   }
 
   /**
@@ -31,7 +25,8 @@ export class Cookie {
   static setCookie(nameC, valueC, expireC) {
     let date = new Date();
     date.setTime(date.getTime() + (expireC * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
+    let expires = "expires=" + date.toUTCString();
+
     document.cookie = nameC + "=" + valueC + "; " + expires + "; path=/";
   }
 
@@ -47,12 +42,30 @@ export class Cookie {
     return matches ? decodeURIComponent(matches[1]) : undefined;
   }
 
-  static disagreeCookie() {
+  static disagreeCookie(elements) {
+    //Click event on btn decline - hide the man div & display the btn re-pop div
     elements.btnDecline.addEventListener('click', (e) => {
-      e.target
+      elements.modalCookie.classList.add('hidden-cookie')
+      elements.modalRePopCookie.classList.remove('hidden-cookie')
+      elements.modalRePopCookie.style.display = 'block';
+
+      setTimeout(() => {
+          elements.modalCookie.style.display = 'none';
+        },
+        700)
     })
 
+    //Click event on btn re-pop div - hide the re-pop div & display the the man div
+    elements.modalRePopCookie.addEventListener('click', (e) => {
+      elements.modalRePopCookie.classList.add('hidden-cookie')
+      elements.modalCookie.classList.remove('hidden-cookie')
+      elements.modalCookie.style.display = 'block';
 
+      setTimeout(() => {
+          elements.modalRePopCookie.style.display = 'none'
+        },
+        400)
+    })
   }
 
 }
